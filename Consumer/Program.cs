b99e9330819a803fb5a0d63e0f9e0a9729146dbb
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-09-08 03:30:54 +0300

    completed

---
+++ Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "sample",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"[x] Received a message : {message}");
+
+                };
+
+                channel.BasicConsume(
+                    queue: "sample",
+                    autoAck : true,
+                    consumer : consumer);
+
+                Console.WriteLine(" Press any key to exit.");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "sample",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                string message = "This is sample message";
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange:"",
+                    routingKey:"sample",
+                    basicProperties:null,
+                    body:body
+                );
+
+                Console.WriteLine($"[x] Sent a message : {message}");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+                
+            
+
+
+        }
+    }
+}
